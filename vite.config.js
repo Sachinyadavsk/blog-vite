abeea2d04ca8b46b318e0e15220dd5a39f1c2f94
Sachinyadavsk <sachinsk4832499@gmail.com>
2025-08-15 15:01:37 +0530

    hh

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Change `repo-name` to your repo when deploying to GitHub Pages
 export default defineConfig({
 plugins: [react()],
-base: process.env.NODE_ENV === 'production' ? '/repo-name/' : '/',
+base: process.env.NODE_ENV === 'production' ? '/blog-vite/' : '/',
 })
